b73c8f6157b8d6d8c7ad7769afea382142dafb1b
Pluk Zwaal <Plukky.zwaal@gmail.com>
2023-05-10 11:38:45 +0200

    Added navbar

---
+++ layouts/navbar.php
@@ -0,0 +1,17 @@
+<div class="navbar">
+    <div class="wrapper">
+        <div class="navbar-content">
+            <a href=""><img src="../img/razer-logo.png" alt=""></a>
+            <a href="">Store</a>
+            <a href="">PC</a>
+            <a href="">Console</a>
+            <a href="">Mobile</a>
+            <a href="">Lifestyle</a>
+            <a href="">Services</a>
+            <a href="">Community</a>
+            <a href="">Support</a>
+            <a href=""><i class="fa fa-search"></i></a>
+            <a href=""><i class="fa fa-shopping-cart"></i></a>
+        </div>
+    </div>
+</div>
